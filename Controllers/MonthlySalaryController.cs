8775300689add6b1138e1a2a65626248cab25a33
vishnuveeraraghavan1905@gmail.com <vishnuveeraraghavan1905@gmail.com>
2024-05-03 12:58:16 +0530

    all files

---
+++ Controllers/MonthlySalaryController.cs
@@ -0,0 +1,29 @@
+﻿using HRMSAPPLICATION.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMSAPPLICATION.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonthlySalaryController : ControllerBase
+    {
+        HrmsystemContext _context;
+        public MonthlySalaryController(HrmsystemContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{year},{month},{empid}")]   
+
+        public Object Get(int year,int month,int empid) {
+
+            Console.WriteLine(year+":"+month+":"+empid);
+
+            var salary = _context.Database.SqlQueryRaw<Double>("EXEC CalculateMonthlySalaryForMonthYear @Year =" + year + ", @Month  =" + month + ", @EmployeeId =" + empid + ";");
+            return salary;
+        
+        }  
+        
+    }
+}
